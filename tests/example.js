49e9b725d992cbc29e2b5c032227c3ba48eea77b
SungbinYang <ysb05222@naver.com>
2021-06-16 22:36:52 +0900

    mock Test

--- tests/example.js
+++ tests/example.js
@@ -1,3 +1,7 @@
+import axios from "axios";
+import _upperFirst from 'lodash/upperFirst';
+import _toLower from 'lodash/toLower';
+
 export function double(num) {
     if (!num) return 0
     return num * 2;
@@ -9,4 +13,10 @@ export function asyncFn() {
             resolve('Done!');
         }, 6000);
     });
+}
+
+export async function fetchMovieTitle() {
+    const res = await axios.get('https://omdbapi.com?apikey=7035c60c&i=tt4520988');
+
+    return _upperFirst(_toLower(res.data.Title)); // Frozen II => Frozen ii
 }
--- tests/example.test.js
+++ tests/example.test.js
@@ -1,111 +1,138 @@
-import { double, asyncFn } from './example';
+import * as example from './example';
+import axios from 'axios';
 
 // 테스트 그룹
-describe('그룹1', () => {
-    beforeAll(() => {
-        console.log('beforeAll!!!');
-    });
-    afterAll(() => {
-        console.log('afterAll!!!');
-    });
+// describe('그룹1', () => {
+//     beforeAll(() => {
+//         console.log('beforeAll!!!');
+//     });
+//     afterAll(() => {
+//         console.log('afterAll!!!');
+//     });
 
-    beforeEach(() => {
-        console.log('beforeEach!!!');
-    });
-    afterEach(() => {
-        console.log('afterEach!!!')
-    });
-    test('첫 테스트', () => {
-        console.log('첫 테스트!');
-        expect(123).toBe(123);
-    });
+//     beforeEach(() => {
+//         console.log('beforeEach!!!');
+//     });
+//     afterEach(() => {
+//         console.log('afterEach!!!')
+//     });
+//     test('첫 테스트', () => {
+//         console.log('첫 테스트!');
+//         expect(123).toBe(123);
+//     });
 
-    test('인수가 숫자 데이터입니다.', () => {
-        console.log('인수가 숫자 데이터입니다!');
-        expect(double(3)).toBe(6);
-        expect(double(10)).toBe(20);
-    });
+//     test('인수가 숫자 데이터입니다.', () => {
+//         console.log('인수가 숫자 데이터입니다!');
+//         expect(double(3)).toBe(6);
+//         expect(double(10)).toBe(20);
+//     });
 
-    test('인수가 없습니다', () => {
-        console.log('인수가 없습니다!');
-        expect(double()).toBe(0);
-    });
-});
+//     test('인수가 없습니다', () => {
+//         console.log('인수가 없습니다!');
+//         expect(double()).toBe(0);
+//     });
+// });
 
-describe('그룹2', () => {
-    beforeAll(() => {
-        console.log('beforeAll!!!');
-    });
-    afterAll(() => {
-        console.log('afterAll!!!');
-    });
+// describe('그룹2', () => {
+//     beforeAll(() => {
+//         console.log('beforeAll!!!');
+//     });
+//     afterAll(() => {
+//         console.log('afterAll!!!');
+//     });
 
-    beforeEach(() => {
-        console.log('beforeEach!!!');
-    });
-    afterEach(() => {
-        console.log('afterEach!!!')
-    });
-    test('첫 테스트', () => {
-        console.log('첫 테스트!');
-        expect(123).toBe(123);
-    });
+//     beforeEach(() => {
+//         console.log('beforeEach!!!');
+//     });
+//     afterEach(() => {
+//         console.log('afterEach!!!')
+//     });
+//     test('첫 테스트', () => {
+//         console.log('첫 테스트!');
+//         expect(123).toBe(123);
+//     });
 
-    test('인수가 숫자 데이터입니다.', () => {
-        console.log('인수가 숫자 데이터입니다!');
-        expect(double(3)).toBe(6);
-        expect(double(10)).toBe(20);
-    });
+//     test('인수가 숫자 데이터입니다.', () => {
+//         console.log('인수가 숫자 데이터입니다!');
+//         expect(double(3)).toBe(6);
+//         expect(double(10)).toBe(20);
+//     });
 
-    test('인수가 없습니다', () => {
-        console.log('인수가 없습니다!');
-        expect(double()).toBe(0);
-    });
-});
+//     test('인수가 없습니다', () => {
+//         console.log('인수가 없습니다!');
+//         expect(double()).toBe(0);
+//     });
+// });
 
-describe('Jest test', () => {
-    const userA = {
-        name: 'Sungbin',
-        age: 26
-    }
+// describe('Jest test', () => {
+//     const userA = {
+//         name: 'Sungbin',
+//         age: 26
+//     }
 
-    const userB = {
-        name: 'Neo',
-        age: 22
-    }
+//     const userB = {
+//         name: 'Neo',
+//         age: 22
+//     }
 
-    test('데이터가 일치해야 합니다', () => {
-        expect(userA.age).toBe(26);
-        expect(userA).toEqual({
-            name: 'Sungbin',
-            age: 26
-        })
-    }, 7000);
+//     test('데이터가 일치해야 합니다', () => {
+//         expect(userA.age).toBe(26);
+//         expect(userA).toEqual({
+//             name: 'Sungbin',
+//             age: 26
+//         })
+//     }, 7000);
 
-    test('데이터가 일치하지 않아야 합니다.', () => {
-        expect(userB.name).not.toBe('Sungbin');
-        expect(userB).not.toEqual(userA);
-    }, 7000);
-});
+//     test('데이터가 일치하지 않아야 합니다.', () => {
+//         expect(userB.name).not.toBe('Sungbin');
+//         expect(userB).not.toEqual(userA);
+//     }, 7000);
+// });
+
+// describe('비동기 테스트', () => {
+//     // test('done', done => {
+//     //     asyncFn().then(res => {
+//     //         expect(res).toBe('Done!');
+//     //         done();
+//     //     });
+//     // }, 7000);
+
+//     // test('then', () => {
+//     //     return asyncFn().then(res => {
+//     //         expect(res).toBe('Done!');
+//     //     });
+//     // }, 7000);
+
+//     // test('resolves', () => expect(asyncFn()).resolves.toBe('Done!'), 7000);
+
+//     test('async/await', async() => {
+//         jest.spyOn(example, 'asyncFn').mockResolvedValue('Done!');
+//         const res = await expample.asyncFn();
+//         expect(res).toBe('Done!');
+//     }, 7000);
+// });
+
+describe('모의함수 테스트', () => {
+    test('async/await', async() => {
+        jest.spyOn(example, 'asyncFn').mockResolvedValue('Done!')
+
+        const res = await example.asyncFn()
+        expect(res).toBe('Done!')
+    }, 7000)
+})
 
 describe('비동기 테스트', () => {
-    test('done', done => {
-        asyncFn().then(res => {
-            expect(res).toBe('Done!');
-            done();
-        });
-    }, 7000);
-
-    test('then', () => {
-        return asyncFn().then(res => {
-            expect(res).toBe('Done!');
-        });
-    }, 7000);
-
-    test('resolves', () => expect(asyncFn()).resolves.toBe('Done!'), 7000);
-
-    test('async/await', async() => {
-        const res = await asyncFn();
-        expect(res).toBe('Done!');
-    }, 7000);
-});
+    test('영화제목 변환', async() => {
+        axios.get = jest.fn(() => {
+            return new Promise(resolve => {
+                resolve({
+                    data: {
+                        Title: 'Frozen II'
+                    }
+                })
+            })
+        })
+        const title = await example.fetchMovieTitle();
+        expect(title).toBe('Frozen ii')
+    })
+})
